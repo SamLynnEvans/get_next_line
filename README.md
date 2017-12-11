f86e1eb121e8cb5e11d6318c9e3f57ebf4f245d8
Sam Lynn-Evans <samuellynnevans@gmail.com>
2017-12-11 12:54:04 +0000

    all files added to repo

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # get_next_line
-C function that can read file line by line
+
+The third project at school 42 requires you to build a function that reads a file up to the next newline character, building a string of the text up to this point, and assigning this string to a pointer given as a parameter. For the full brief; get_next_line.en.pdf.
+
+The function is dependent on usage of functions created in our first project ‘libft’, which is included also in this repository. 
+
+The challenge in this project was to understand how the C function ‘read’ works. Read will read a predetermined number of bytes at a time, hence often will read beyond the first newline character.
+
+My version uses a linked list to build the string produced by calls to read. Then when a new-line character is found, its index is stored, and the string before this index is assigned to *line. The string after is assigned to the static char pointer *leftover, which is checked during successive calls to get_next_line before calling read.
+
+Prototype: 
+
+int	get_next_line(int fd, char **line)
+
+Return value:
+
+Get next line returns (1) if there is still more of the file to read, (0) if the whole file has been read, and (-1) if there are any errors.
---
+++ get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slynn-ev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/06 11:59:20 by slynn-ev          #+#    #+#             */
+/*   Updated: 2017/12/07 21:01:36 by slynn-ev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 7
+# define OPEN_MAX 1000
+# define EMPTY_LINE 2
+# define GET_NEXT_LINE 3
+# define NO_NL -1
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+typedef struct		s_gets
+{
+	char		*line_content;
+	char		*leftover;
+	char		*send_up;
+	int		content_size;
+	int		nl_total;
+	int		end;
+}			t_gets;
+
+int	get_next_line(int fd, char **line);
+
+#endif
